ce6e1f16028eaa5c731b49f9b9dceda49e5e8270
Fabien JUIF <fabien.juif@zenika.com>
2016-03-18 12:21:25 +0100

    New level and fix the Formation one

--- assets/Scripts (7)/Managers (72)/LevelManager (38)/script.ts
+++ assets/Scripts (7)/Managers (72)/LevelManager (38)/script.ts
@@ -4,7 +4,7 @@ class LevelManager {
   private levels:string[] = [
     "Scenes/PlayerChoice",
     "Scenes/SlackName",
-    "Scenes/Tutorial",
+    // "Scenes/Tutorial",
     "Scenes/Presentation",
     "Scenes/Formation",
     //"Scenes/Experience",
