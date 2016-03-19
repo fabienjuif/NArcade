a4a5bcd94b61f2339b40deb47e32c4e1dbab5e5a
Fabien JUIF <fabien.juif@zenika.com>
2016-03-19 23:20:01 +0100

    Auto saving

--- assets/Scripts (7)/Scenes (120)/Formation (121)/script.ts
+++ assets/Scripts (7)/Scenes (120)/Formation (121)/script.ts
@@ -8,12 +8,12 @@ class FormationBehavior extends Sup.Behavior {
     Sup.getActor("MovingPlatform4").getBehavior(MovingPlatformBehavior).min = -9;
     Sup.getActor("MovingPlatform4").getBehavior(MovingPlatformBehavior).max = -1;
     Sup.getActor("MovingPlatform4").getBehavior(MovingPlatformBehavior).speed = 0.06;
-    Sup.getActor("MovingPlatform4").getBehavior(MovingPlatformBehavior).direction = true;
+    Sup.getActor("MovingPlatform4").getBehavior(MovingPlatformBehavior).direction = false;
     
     Sup.getActor("MovingPlatform5").getBehavior(MovingPlatformBehavior).min = -9;
     Sup.getActor("MovingPlatform5").getBehavior(MovingPlatformBehavior).max = -2;
     Sup.getActor("MovingPlatform5").getBehavior(MovingPlatformBehavior).speed = 0.06;
-    Sup.getActor("MovingPlatform5").getBehavior(MovingPlatformBehavior).direction = true;
+    Sup.getActor("MovingPlatform5").getBehavior(MovingPlatformBehavior).direction = false;
     
     Sup.getActor("MovingPlatform2").getBehavior(MovingPlatformBehavior).min = -9;
     Sup.getActor("MovingPlatform2").getBehavior(MovingPlatformBehavior).max = 4;
