590eb3d13eb4f192e5ab144939a8f9afe8f2f4ff
Fabien JUIF <fabien.juif@zenika.com>
2016-03-19 23:14:03 +0100

    Auto saving

--- assets/Scripts (7)/Scenes (120)/Formation (121)/script.ts
+++ assets/Scripts (7)/Scenes (120)/Formation (121)/script.ts
@@ -1,28 +1,33 @@
 class FormationBehavior extends Sup.Behavior {
   awake() {
-    Sup.getActor("MovingPlatform1").getBehavior(MovingPlatformBehavior).min = -28;
-    Sup.getActor("MovingPlatform1").getBehavior(MovingPlatformBehavior).max = -8;
+    Sup.getActor("MovingPlatform1").getBehavior(MovingPlatformBehavior).min = 12;
+    Sup.getActor("MovingPlatform1").getBehavior(MovingPlatformBehavior).max = 18;
     Sup.getActor("MovingPlatform1").getBehavior(MovingPlatformBehavior).speed = 0.06;
-    Sup.getActor("MovingPlatform1").getBehavior(MovingPlatformBehavior).direction = false;
+    Sup.getActor("MovingPlatform1").getBehavior(MovingPlatformBehavior).direction = true;
     
-    Sup.getActor("MovingPlatform2").getBehavior(MovingPlatformBehavior).min = -20;
-    Sup.getActor("MovingPlatform2").getBehavior(MovingPlatformBehavior).max = -7;
+    Sup.getActor("MovingPlatform4").getBehavior(MovingPlatformBehavior).min = -9;
+    Sup.getActor("MovingPlatform4").getBehavior(MovingPlatformBehavior).max = -1;
+    Sup.getActor("MovingPlatform4").getBehavior(MovingPlatformBehavior).speed = 0.06;
+    Sup.getActor("MovingPlatform4").getBehavior(MovingPlatformBehavior).direction = true;
+    
+    Sup.getActor("MovingPlatform5").getBehavior(MovingPlatformBehavior).min = -9;
+    Sup.getActor("MovingPlatform5").getBehavior(MovingPlatformBehavior).max = -2;
+    Sup.getActor("MovingPlatform5").getBehavior(MovingPlatformBehavior).speed = 0.06;
+    Sup.getActor("MovingPlatform5").getBehavior(MovingPlatformBehavior).direction = true;
+    
+    Sup.getActor("MovingPlatform2").getBehavior(MovingPlatformBehavior).min = -9;
+    Sup.getActor("MovingPlatform2").getBehavior(MovingPlatformBehavior).max = 4;
     Sup.getActor("MovingPlatform2").getBehavior(MovingPlatformBehavior).speed = 0.06;
-    Sup.getActor("MovingPlatform2").getBehavior(MovingPlatformBehavior).direction = true;
+    Sup.getActor("MovingPlatform2").getBehavior(MovingPlatformBehavior).direction = false;
     
-    Sup.getActor("MovingPlatform3").getBehavior(MovingPlatformBehavior).min = -28;
-    Sup.getActor("MovingPlatform3").getBehavior(MovingPlatformBehavior).max = -22;
+    Sup.getActor("MovingPlatform3").getBehavior(MovingPlatformBehavior).min = 15;
+    Sup.getActor("MovingPlatform3").getBehavior(MovingPlatformBehavior).max = 22;
     Sup.getActor("MovingPlatform3").getBehavior(MovingPlatformBehavior).speed = 0.06;
     Sup.getActor("MovingPlatform3").getBehavior(MovingPlatformBehavior).direction = false;
     
-    Sup.getActor("MovingPlatform4").getBehavior(MovingPlatformBehavior).min = -27;
-    Sup.getActor("MovingPlatform4").getBehavior(MovingPlatformBehavior).max = -18;
-    Sup.getActor("MovingPlatform4").getBehavior(MovingPlatformBehavior).speed = 0.06;
-    Sup.getActor("MovingPlatform4").getBehavior(MovingPlatformBehavior).direction = false;
   }
 
   update() {
-    Sup.log(Sup.getActor("MovingPlatform1").getPosition());
   }
 }
 Sup.registerBehavior(FormationBehavior);
