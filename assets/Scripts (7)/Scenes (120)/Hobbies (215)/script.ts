76334ab3adb5fdba611c339553ce9bee47c0702c
Fabien JUIF <fabien.juif@zenika.com>
2016-03-20 11:20:01 +0100

    Auto saving

--- assets/Scripts (7)/Scenes (120)/Hobbies (215)/script.ts
+++ assets/Scripts (7)/Scenes (120)/Hobbies (215)/script.ts
@@ -8,6 +8,16 @@ class HobbiesBehavior extends Sup.Behavior {
     Sup.getActor("MovingPlatform1").getBehavior(MovingPlatformBehavior).max = 8;
     Sup.getActor("MovingPlatform1").getBehavior(MovingPlatformBehavior).speed = 0.06;
     Sup.getActor("MovingPlatform1").getBehavior(MovingPlatformBehavior).direction = horizontal;
+    
+    Sup.getActor("SlackMovingPlatform1").getBehavior(MovingPlatformBehavior).min = -14,5;
+    Sup.getActor("SlackMovingPlatform1").getBehavior(MovingPlatformBehavior).max = -9;
+    Sup.getActor("SlackMovingPlatform1").getBehavior(MovingPlatformBehavior).speed = 0.06;
+    Sup.getActor("SlackMovingPlatform1").getBehavior(MovingPlatformBehavior).direction = horizontal;
+    
+    Sup.getActor("SlackMovingPlatform2").getBehavior(MovingPlatformBehavior).min = -11;
+    Sup.getActor("SlackMovingPlatform2").getBehavior(MovingPlatformBehavior).max = -8;
+    Sup.getActor("SlackMovingPlatform2").getBehavior(MovingPlatformBehavior).speed = 0.06;
+    Sup.getActor("SlackMovingPlatform2").getBehavior(MovingPlatformBehavior).direction = vertical;
   }
 
   update() {
