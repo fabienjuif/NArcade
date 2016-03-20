23478baf3a64708224138063109ee41b8a359297
Fabien JUIF <fabien.juif@zenika.com>
2016-03-20 01:00:01 +0100

    Auto saving

--- assets/Scripts (7)/Scenes (120)/Hobbies (215)/script.ts
+++ assets/Scripts (7)/Scenes (120)/Hobbies (215)/script.ts
@@ -8,9 +8,6 @@ class HobbiesBehavior extends Sup.Behavior {
     Sup.getActor("MovingPlatform1").getBehavior(MovingPlatformBehavior).max = 8;
     Sup.getActor("MovingPlatform1").getBehavior(MovingPlatformBehavior).speed = 0.06;
     Sup.getActor("MovingPlatform1").getBehavior(MovingPlatformBehavior).direction = horizontal;
-    
-    // Destroy the slack platform at the end
-    Sup.getActor("ToFix").getBehavior(DestroyBehaviour).actionDestroy();
   }
 
   update() {
